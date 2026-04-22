4ad7e7a459199ed11d1a736787343a4cc55b52e3
FELIPE FERREIRA DA SILVA <2993620@fmu.interno>
2026-04-22 19:45:14 -0300

    feat: initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+// Aplicativo focado em cadastro de clientes de conta digital/cartão
+
+// DOD
+// Ter uma página na web para consulta, cadastro, edição e deleção de clientes
+// Página no formato de ChatBot (conversa/atendimento)
+// Consulta baseado em CPF
+// Retorno de informações e opções de gerenciamento de conta
+
+// Usar a biblioteca Flask para rodar na web - Será a nota da APS - Até dia 06/05
+// Usar sqlite para armazenamento de dados
+
+// Modelagem de tabela
+// 
